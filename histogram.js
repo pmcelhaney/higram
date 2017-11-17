169b54d4063ec26cea44c9ea24d993cf9c2ecf21
Patrick McElhaney <Patrick.McElhaney@fisglobal.com>
2017-11-16 19:18:04 -0500

    Histogram.toStream()

--- histogram.js
+++ histogram.js
@@ -1,3 +1,5 @@
+const Readable = require('stream').Readable;
+
 module.exports = class Histogram {
 
     constructor () {
@@ -12,4 +14,22 @@ module.exports = class Histogram {
         return this.counts;
     }
 
+    toStream() {
+        const stream = new Readable();
+        const data = [];
+
+        for (let name in this.counts) {
+            data.push({name, count: this.counts[name] });
+        }
+
+        data.sort((a, b) => b.count - a.count);
+
+        data.forEach(item => {
+            stream.push(`${item.count} ${item.name}\n`);
+        })
+
+        stream.push(null);
+        return stream;
+
+    }
 }
